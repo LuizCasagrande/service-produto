89ce1d871f9419048fc32e64b9176febb72a9bec
luizcgd <casagrandehluiz@gmail.com>
2021-11-08 21:13:05 -0300

    #4

--- src/main/java/com/luizcasagrande/serviceproduto/http/ProdutoControllerImpl.java
+++ src/main/java/com/luizcasagrande/serviceproduto/http/ProdutoControllerImpl.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("produto")
+@RequestMapping("/")
 public class ProdutoControllerImpl implements ProdutoController {
 
     private final ProdutoService produtoService;
